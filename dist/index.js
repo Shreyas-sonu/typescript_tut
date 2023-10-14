59fdcd7a3aa08a96de52483166893f35fad1331d
Shreyas <sonureigns007@gmail.com>
2023-10-14 21:50:47 +0530

    hello world

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,5 @@
 let age = 20;
 age = 20;
 console.log("hello world");
+let message = "Hello, World!";
+console.log(message);
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ age = 20;
 // age = '1';
 console.log("hello world");
 // const num: int = 1;
+let message: string = "Hello, World!";
+console.log(message);
