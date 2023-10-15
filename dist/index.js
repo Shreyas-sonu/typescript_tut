15d0cdd897ae2977baf6c301b84f2cc706e946da
Shreyas <sonureigns007@gmail.com>
2023-10-15 20:05:28 +0530

    function parameter declaration in ts

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,9 @@ age = 20;
 console.log("hello world");
 let message = "Hello, World!";
 console.log(message);
+let hello = (a, b) => {
+    console.log(a);
+    console.log('hello');
+    console.log(b);
+};
+hello(5, 'Sonu');
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,9 @@ console.log("hello world");
 // const num: int = 1;
 let message: string = "Hello, World!";
 console.log(message);
+let hello = (a:number,b:string) => {
+    console.log(a);
+    console.log('hello');
+    console.log(b);
+}
+hello(5,'Sonu');
