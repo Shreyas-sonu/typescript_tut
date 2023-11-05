24937c70aa9f7f7d4ab364acfe29852d59ec9917
Shreyas <sonureigns007@gmail.com>
2023-11-05 22:09:35 +0530

    optional property in ts object

--- src/extra.ts
+++ src/extra.ts
@@ -3,8 +3,14 @@
 //   model: "Corolla",
 //   year: 2009,
 // };
-const car = {
+// const car = {
+//   type: "Toyota",
+// };
+// car.type = "Ford"; // no error
+// car.type = 2; // Error: Type 'number' is not assignable to type 'string'.
+
+const car: { type: string; mileage: number } = {
+  // Error: Property 'mileage' is missing in type '{ type: string; }' but required in type '{ type: string; mileage: number; }'.
   type: "Toyota",
 };
-car.type = "Ford"; // no error
-car.type = 2; // Error: Type 'number' is not assignable to type 'string'.
+car.mileage = 2000;
